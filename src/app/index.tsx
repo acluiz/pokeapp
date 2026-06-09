e28f904c4f383ec4a93e1ed76a06e797a3ba018a
Ana Luiz <acluiz.study@gmail.com>
2026-06-08 22:32:27 -0300

    feat: add header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
-import { View } from "react-native";
+import { Image, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Button, ButtonIcon } from "@/components/ui/button";
@@ -8,7 +8,16 @@ import { SearchIcon } from "@/components/ui/icon";
 export default function App() {
   return (
     <SafeAreaView className="flex-1">
-      <View className="flex-row gap-4 px-4">
+      <View className="px-4 py-3 border-b border-gray-200">
+        <Image
+          width={165}
+          height={38}
+          resizeMode="contain"
+          source={require("@assets/images/logo.png")}
+        />
+      </View>
+
+      <View className="flex-row gap-4 px-4 mt-4">
         <Input size="xl" variant="outline" className="flex-1">
           <InputSlot className="pl-3">
             <InputIcon as={SearchIcon} />
